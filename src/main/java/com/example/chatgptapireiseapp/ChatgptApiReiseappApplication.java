5e46e677daf6a74972c74b2a7d67017dffa7e533
Daniel Schwarz <bartfastiel@users.noreply.github.com>
2023-10-20 08:42:11 +0200

    Setup project (spring initializr lombok+web+webflux)

---
+++ src/main/java/com/example/chatgptapireiseapp/ChatgptApiReiseappApplication.java
@@ -0,0 +1,13 @@
+package com.example.chatgptapireiseapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatgptApiReiseappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChatgptApiReiseappApplication.class, args);
+    }
+
+}
